d51a2a57ad1fc3aa249f69a8fee5ca75c8815e06
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-06-29 14:25:06 +0200

    Upload mutation testing results to Stryker dashboard

---
+++ .github/upload-mutation-report.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -Euo pipefail
+
+trap "rm mutation-testing-report.json" ERR
+
+reportJsLocation=$(find . -name "report.js")
+echo Found report.js at ${reportJsLocation}
+reportJsContent=$(cat ${reportJsLocation})
+report=${reportJsContent:60}
+echo ${report} > mutation-testing-report.json
+
+BASE_URL="https://dashboard.stryker-mutator.io"
+PROJECT=${GITHUB_REPOSITORY}
+VERSION=${GITHUB_REF#refs/heads/}
+
+echo Uploading mutation-testing-report.json to ${BASE_URL}/api/reports/${PROJECT}/${VERSION}
+curl -X PUT \
+  ${BASE_URL}/api/reports/${PROJECT}/${VERSION} \
+  -H "Content-Type: application/json" \
+  -H "Host: dashboard.stryker-mutator.io" \
+  -H "X-Api-Key: ${API_KEY}" \
+  -d @mutation-testing-report.json
+
+rm mutation-testing-report.json
