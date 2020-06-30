42e06427f5706e5a737d4391b181109eeeecc80d
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-06-30 13:01:04 +0200

    Remove verbose switch from cURL invocation

--- .github/upload-mutation-report.sh
+++ .github/upload-mutation-report.sh
@@ -14,7 +14,7 @@ PROJECT="github.com/${GITHUB_REPOSITORY}"
 VERSION=${GITHUB_REF#refs/heads/}
 
 echo Uploading mutation-testing-report.json to ${BASE_URL}/api/reports/${PROJECT}/${VERSION}
-curl -v -X PUT \
+curl -X PUT \
   ${BASE_URL}/api/reports/${PROJECT}/${VERSION} \
   -H "Content-Type: application/json" \
   -H "Host: dashboard.stryker-mutator.io" \
